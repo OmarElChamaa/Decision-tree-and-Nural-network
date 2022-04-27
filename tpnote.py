92d02ee9019da1b5b760f37b661ee4e03b56a03a
Droober67 <chebblidisdier>
2022-04-27 16:25:02 +0200

    essai entropie/gain info pour arbre, pas fameux

--- tpnote.py
+++ tpnote.py
@@ -12,11 +12,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from math import log2
 
 df = pd.read_csv("synthetic.csv")
-
+"""
 nb0 = df['Class'].value_counts()
 print(nb0)
+"""
 """
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
@@ -24,12 +26,17 @@ ax.scatter(df['Attr_K'], df['Attr_L'])
 plt.xlabel('Attr_K')
 plt.ylabel('Attr_L')
 """
+print(df)
 
+test = df.groupby(['Class','Attr_A'])
+test.size()
+
+"""
 maxc = df['Attr_N'].loc[df['Attr_N'].idxmax()]
 minc = df['Attr_N'].loc[df['Attr_N'].idxmin()]
 print(minc)
 print(maxc)
-"""
+
 A : min 3.84 max 17.63
 B : min 3.73 max 16.26
 C : min 409.40 max 1725.27
@@ -44,4 +51,52 @@ K : min 666.06 max 1369.16
 L : min 6.34 max 13.34
 M : min 654.66 max 1361.13
 N : min 34.54 max 155.13
+
+Recuperer une valeur associee a une etiquette donnee
+occ_setosa = series . get ( ’ Iris - setosa ’)
 """
+
+"""
+2. Arbre de décision
+"""
+
+def entropie_df(df):
+    res = 0
+    nbl = df.shape[0]
+    maxdf = df['Class'].loc[df['Class'].idxmax()]
+    for ind in range(maxdf+1):
+        val = df[df.Class == ind].shape[0]/nbl
+        res -= val * log2(val)
+    return res    
+
+def info_gain(df, i):
+    sump = 0
+    ent = entropie_df(df)
+    nbl = shape[0]
+    p = df[df.Class == ind].shape[0]/nbl
+    maxdf = df['Class'].loc[df['Class'].idxmax()]
+    for ind in range (maxdf+1):
+        part = df[df.Class == ind]
+        sump += (p/nbl)*entropie_df(part)
+        print(sump)
+        print(entropie_df(part))
+    gain = ent - sump
+    print(gain)
+
+ent = entropie_df(df)
+info_gain()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
