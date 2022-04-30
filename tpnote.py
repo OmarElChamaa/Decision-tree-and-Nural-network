fa79e54a2b7760b45f0594d79a3e76064f3433fb
EL-CHAMAA OMAR <chamaael@etu.unistra.fr>
2022-04-30 20:57:19 +0200

    Gain et entropie fonctionnels

--- tpnote.py
+++ tpnote.py
@@ -60,31 +60,79 @@ occ_setosa = series . get ( ’ Iris - setosa ’)
 2. Arbre de décision
 """
 
-def entropie_df(df):
-    res = 0
-    nbl = df.shape[0]
-    maxdf = df['Class'].loc[df['Class'].idxmax()]
-    for ind in range(maxdf+1):
-        val = df[df.Class == ind].shape[0]/nbl
-        res -= val * log2(val)
-    return res    
+# def entropie_df(df):
+#     res = 0
+#     nbl = df.shape[0]
+#     maxdf = df['Class'].loc[df['Class'].idxmax()]
+#     for ind in range(maxdf+1):
+#         val = df[df.Class == ind].shape[0]/nbl
+#         res -= val * log2(val)
+#     return res    
 
-def info_gain(df, i):
+
+def entropie_df(df) : 
+    nb_lignes = df.shape[0]
+    series = df['Class'].value_counts()
+    res = 0 
+    for i in series :
+        p = i/nb_lignes
+        res -= p*log2(p)
+    return res 
+
+print( entropie_df(df))
+data_sorted = df.sort_values(by ='Attr_A')
+print(data_sorted)
+
+def info_gain(df, a):
     sump = 0
     ent = entropie_df(df)
-    nbl = shape[0]
-    p = df[df.Class == ind].shape[0]/nbl
-    maxdf = df['Class'].loc[df['Class'].idxmax()]
-    for ind in range (maxdf+1):
-        part = df[df.Class == ind]
-        sump += (p/nbl)*entropie_df(part)
-        print(sump)
-        print(entropie_df(part))
-    gain = ent - sump
-    print(gain)
+    split_value = 0 
+    partitions = [None,None]
+    sorted_data = df.sort_values(by = a)
+    classe = sorted_data['Class'].iloc[0]
+    for i in range (len(sorted_data)) : 
+        if sorted_data["Class"].iloc[i] != classe : 
+            split_value = sorted_data[a].iloc[i]
+            partitions[0] = sorted_data.iloc[:i]
+            partitions[1] = sorted_data.iloc[i:]
+            sump = 0 
+            for x in partitions :
+                sump += len(x)/len(df) * entropie_df (x)
+            gain = ent - sump 
+            break
+    return gain , split_value , partitions
 
-ent = entropie_df(df)
-info_gain()
+def best_attribute(df,attributes) :
+    max_gain = -1 
+    partitions = []
+    max_split = 0 
+    attribute = ''
+    for i in range (len(attributes)) :
+        gain,split,tmpPartitions = info_gain(df,attributes[i])
+        if(gain>max_gain):
+            max_gain = gain 
+            partitions = tmpPartitions
+            max_split = split 
+            attribute = attributes[i]
+    return attribute,max_gain,max_split,partitions
+
+print('\n\n\033 [1mGain : \033[0m',info_gain(df,'Attr_A')[0])
+print('\n\n\033 [1mMeilleur Gain : \033[0m',best_attribute(df,df.columns.tolist()[0:4])[0])
+
+
+    # nbl = shape[0]
+    # p = df[df.Class == ind].shape[0]/nbl
+    # maxdf = df['Class'].loc[df['Class'].idxmax()]
+    # for ind in range (maxdf+1):
+    #     part = df[df.Class == ind]
+    #     sump += (p/nbl)*entropie_df(part)
+    #     print(sump)
+    #     print(entropie_df(part))
+    # gain = ent - sump
+    # print(gain)
+
+# ent = entropie_df(df)
+# info_gain()
 
 
 
